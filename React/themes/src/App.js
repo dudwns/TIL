61b301ce2fb7431ed116347f7844ee833086af8b
yeongjun Kim <ehdwjs500dnj@naver.com>
2022-11-23 00:59:25 +0900

    Themes

---
+++ React/themes/src/App.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+
+const Wrapper = styled.div`
+display:flex;
+height:100vh;
+width:100vw;
+justify-content:center;
+align-items:center;
+  background-color: ${(props) => props.theme.backgroundColor}; //Theme 적용
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor}; //Theme 적용
+`;
+
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>
+  );
+}
+
+export default App;
