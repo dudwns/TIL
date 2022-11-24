8772a616fcf52834ef429c40710df9d908916353
yeongjun Kim <ehdwjs500dnj@naver.com>
2022-11-24 20:25:12 +0900

    Themes

--- React/themes/src/index.js
+++ React/themes/src/index.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ThemeProvider} from "styled-components";     //Theme를 쓰려면 import 해야 함
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "styled-components"; //Theme를 쓰려면 import 해야 함
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const darkTheme = {
-  textColor:"whitesmoke",
-  backgroundColor:"#111",
-}
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
 
 const lightTheme = {
-  textColor:"#111",
-  backgroundColor:"whitesmoke",
-}
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>  {/*<ThemeProvider>로 감싸줘야 됨, theme prop만 바꾸면 모드 변경 가능 */}
+    {/*<ThemeProvider>로 감싸줘야 됨, theme prop만 바꾸면 모드 변경 가능 */}
+    <ThemeProvider theme={darkTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
 );
-
