793c552c119171f5264b0a1db58cb5ab56d8cc75
yeongjun Kim <ehdwjs500dnj@naver.com>
2022-11-27 16:43:14 +0900

    Update Coin page

--- React/crypto-tracker/src/routes/Coin.tsx
+++ React/crypto-tracker/src/routes/Coin.tsx
@@ -151,9 +151,10 @@ function Coin() {
       const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
       const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
       setInfo(infoData);
+      setPriceInfo(priceData);
       setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
