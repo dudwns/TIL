50c4b80b303f2e6fa56b5590f99303cad9b24fc7
yeongjun Kim <ehdwjs500dnj@naver.com>
2022-12-04 02:41:22 +0900

    To Do List

--- React/to-do/src/atoms.tsx
+++ React/to-do/src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   text: string;
@@ -9,4 +9,17 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    //get function을 이용하면 selector의 내부로 atom을 가지고 올 수 있음
+    const toDos = get(toDoState); //atom을 가져옴
+    return [
+      toDos.filter((toDo) => toDo.category === "TO_DO"),
+      toDos.filter((toDo) => toDo.category === "DOING"),
+      toDos.filter((toDo) => toDo.category === "DONE"),
+    ];
+  },
 });
