84743957fdc3ee260caa8012afff427e3e07a8cd
yeongjun Kim <ehdwjs500dnj@naver.com>
2022-12-03 13:00:00 +0900

    To Do List

--- React/to-do/src/components/ToDo.tsx
+++ React/to-do/src/components/ToDo.tsx
@@ -1,12 +1,42 @@
-import { IToDo } from "../atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+
+    setToDos((prev) =>
+      prev.map((oldToDo) => {
+        if (oldToDo.id === id) {
+          return { text, id, category: name as any };
+        }
+        return oldToDo;
+      })
+    );
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {/*DOING이 아닐때만 Doing 버튼이 보이게*/}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
