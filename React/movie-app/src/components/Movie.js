a57979727c8828b7b0229ec124b23ba56489dfbf
yeongjun Kim <ehdwjs500dnj@naver.com>
2022-12-05 00:27:21 +0900

    Movie App

--- React/movie-app/src/components/Movie.js
+++ React/movie-app/src/components/Movie.js
@@ -1,30 +1,33 @@
 import PropTypes from "prop-types";
-import {Link} from "react-router-dom"; //브라우저의 새로고침 없이 유저를 다른 페이지로 이동시켜주는 컴포넌트
+import { Link } from "react-router-dom"; //브라우저의 새로고침 없이 유저를 다른 페이지로 이동시켜주는 컴포넌트
+import styles from "./Movie.module.css";
 
-function Movie ({id, coverImg, title, summary, genres}) {
-    return (
-        <div>
-            <img src={coverImg} alt={title} />
-            <h2>
-                <Link to={`/movie/${id}`}>{title}</Link>  {/*<a>태그 대신 <Link>를 사용 (<a>태그를 사용하면 리액트의 장점이 사라짐) */}
-            </h2>
-            <p>{summary}</p>
-            <ul>
-                {genres.map((g) => (
-                <li key={g}>{g}</li>
-                ))}
-            </ul> 
-        </div>
-    );
+function Movie({ id, coverImg, title, year, summary, genres }) {
+  return (
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link> {/*<a>태그 대신 <Link>를 사용 (<a>태그를 사용하면 리액트의 장점이 사라짐) */}
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-    id:PropTypes.number.isRequired,
-    coverImg : PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-
-}
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Movie;
--- React/movie-app/src/routes/Detail.js
+++ React/movie-app/src/routes/Detail.js
@@ -1,43 +1,53 @@
-import {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
 
 function Detail() {
-    const [loading, setLoading] = useState([true]);
-    const [movies, setMovies] = useState([]);
-    const {id} = useParams();
-    const getMovie = async () =>{
-        const json = await (
-            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-            ).json();
-            setMovies(json.data.movie);
-            setLoading(false);
-    }
-    useEffect(() => {
-        getMovie();
-    },[])
-    return (
-        <div>
-            {loading ? <h1>Loading...</h1> : 
-            <div>
-                <img src={movies.medium_cover_image}/>
-                <ul>
-                     <li><strong>id:</strong> {movies.id}</li>
-                     <li><strong>title:</strong> {movies.title}</li>
-                     <li><strong>description:</strong> {movies.description_full}</li>
-                     <li><strong>rating:</strong> {movies.rating}</li>            
-                     <li>
-                        <strong>genres</strong>
-                        <ul>
-                            {movies.genres.map((g) => (
-                            <li key={g}>{g}</li>
-                            ))}
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            }
+  const [loading, setLoading] = useState([true]);
+  const [movies, setMovies] = useState([]);
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+    setMovies(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div className={styles.container}>
+      {loading ? (
+        <div className={styles.loader}>
+          <span>Loading...</span>
         </div>
-    )
+      ) : (
+        <div className={styles.detail}>
+          <img src={movies.medium_cover_image} />
+          <ul>
+            <li>
+              <strong>id:</strong> {movies.id}
+            </li>
+            <li>
+              <strong>title:</strong> {movies.title}
+            </li>
+            <li>
+              <strong>description:</strong> {movies.description_full.length > 150 ? `${movies.description_full.slice(0, 150)}...` : movies.description_full}
+            </li>
+            <li>
+              <strong>rating:</strong> {movies.rating}
+            </li>
+            <li>
+              <strong>genres</strong>
+              <ul>
+                {movies.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
