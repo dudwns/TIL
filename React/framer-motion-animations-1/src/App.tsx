2075c2c5a046764f7aaff2f2de3aac4fb4f0fcfa
yeongjun Kim <ehdwjs500dnj@naver.com>
2022-12-12 01:00:51 +0900

    Framer Motion

--- React/framer-motion-animations-1/src/App.tsx
+++ React/framer-motion-animations-1/src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -36,6 +37,25 @@ const Circle = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const BiggerBox = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const Box2 = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
 const myVars = {
   start: { scale: 0 },
   end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
@@ -67,7 +87,14 @@ const circleVariants = {
   },
 };
 
+const boxVariants2 = {
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
+};
+
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null); // BiggerBox 객체를 가져옴
   return (
     <Wrapper>
       <Box variants={myVars} initial="start" animate="end" />
@@ -78,6 +105,18 @@ function App() {
         <Circle variants={circleVariants} />
         <Circle variants={circleVariants} />
       </Box1>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box2
+          drag //드래그 활성화, drag="x" 는 x축으로만 드래그 가능, y도 같음
+          dragConstraints={biggerBoxRef} // 드래깅이 허용될 수 있는 영역 , 레퍼런스 참조값을 연결
+          dragSnapToOrigin // 드래그한 후 원래 자리로 돌아옴
+          dragElastic={0.5} //끌어당기는 힘(탄성), 0~1 사이의 값, default는 0.5
+          variants={boxVariants2}
+          whileHover="hover" //마우스를 올렸을 때 발생하는 이벤트
+          whileTap="click" //클릭했을 때 발생하는 이벤트
+          whileDrag="drag" //드래그할 때 발생하는 이벤트, 색은 rgb값이어야 애니메이션이 작동
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
@@ -90,12 +129,4 @@ export default App;
 // - mass는 무게, bounce는 튕김도(0~1사이로 지정)
 //initial: animation의 초기 스타일 지정
 //animate: animation의 최종 스타일 지정
-//<Box transition={{ delay: 3, duration: 3 }} animate={{ borderRadius: "100px" }} />
-{
-  /* <Box
-        transition={{ type: "spring", delay: 0.5 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      /> */
-}
 //Variants: 컴포넌트가 가질 수 있는 미리 정의된 시각적 state
