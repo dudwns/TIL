d45791d0e2b6e9f48562d5036d21527aa4ace7be
yeongjun Kim <ehdwjs500dnj@naver.com>
2022-12-13 11:09:29 +0900

    AnimatePresence

---
+++ React/framer-motion-animations-4/src/App.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+const Wrapper = styled(motion.div)`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 200px;
+  background-color: white;
+  border-radius: 15px;
+  position: absolute;
+  top: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+  },
+  leaving: {
+    opacity: 0,
+    scale: 0,
+    y: 50,
+  },
+};
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const toggleShowing = () => {
+    setShowing((prev) => !prev);
+  };
+  return (
+    <Wrapper>
+      <button onClick={toggleShowing}>Click</button>
+      <AnimatePresence>
+        {showing ? (
+          <Box variants={boxVariants} initial="initial" animate="visible" exit="leaving" />
+        ) : null}
+      </AnimatePresence>
+    </Wrapper>
+  );
+}
+
+export default App;
+// AnimatePresence: 컴포넌트가 제거될 때 제거되는 컴포넌트에 애니메이션 효과를 줄 수 있다.
+// exit는 컴포넌트가 사라질 때의 애니메이션
