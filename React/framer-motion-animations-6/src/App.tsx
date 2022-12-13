61fea4ba09fc8d4f529c66e0ff70387a19a3d4df
yeongjun Kim <ehdwjs500dnj@naver.com>
2022-12-14 02:46:24 +0900

    Layout and LayoutId

---
+++ React/framer-motion-animations-6/src/App.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+const Wrapper = styled(motion.div)`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 400px;
+  background-color: white;
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+function App() {
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
+  return (
+    <Wrapper onClick={toggleClicked}>
+      <Box>{!clicked ? <Circle layoutId="circle" style={{ borderRadius: 50 }} /> : null}</Box>
+      <Box>
+        {clicked ? <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} /> : null}
+      </Box>
+    </Wrapper>
+  );
+}
+
+export default App;
+
+//layout: 레이아웃이 변경 될 때 자동으로 애니메이션을 적용
+//layoutId: 서로 다른 컴포넌트를 같은 컴포넌트로 연결시켜 애니메이션을 적용
