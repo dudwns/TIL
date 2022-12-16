5d05e97c34f51817ac7c9d8697ae47c220a1f194
yeongjun Kim <ehdwjs500dnj@naver.com>
2022-12-16 21:35:43 +0900

    Add Header

--- React/junflix/src/App.tsx
+++ React/junflix/src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route> {/*라우터 버전 5에서는 맨 밑으로 내려야 함 */}
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
