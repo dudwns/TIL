06cbfe0e17c7411c00c5a161231496a1d8cff802
yeongjun Kim <ehdwjs500dnj@naver.com>
2023-01-08 22:08:57 +0900

    Add User interface

--- JavaScript/Chrome/app.js
+++ JavaScript/Chrome/app.js
@@ -8,13 +8,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"; //string만 포함된 변수는 대문자로 표기 + 중요한 변수가 아니라서
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault(); //이벤트의 기본 동작을 막는다.
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username); //로컬 스토리지에 값을 저장 (키, 값)
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`; //"Hello " + username와 같은 방식 (string이랑 변수를 하나로 합쳐준다.)
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
