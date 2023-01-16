3bf9ecc6c2dd50aeab7f8be0a18e7c9d46086028
yeongjun Kim <ehdwjs500dnj@naver.com>
2023-01-16 23:34:35 +0900

    react-redux

--- Redux/react-redux/src/store.js
+++ Redux/react-redux/src/store.js
@@ -14,7 +14,7 @@ const deleteToDo = (id) => {
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
+      return [{ text: action.text, id: Date.now() }, ...state]; //mutate state는 금지, 기존의 배열에서 추가하는 것이 아닌 새로운 배열을 리턴
     case DELETE:
       return state.filter((toDo) => toDo.id !== action.id);
     default:
