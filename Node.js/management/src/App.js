908fe9cbfc9fcf46bb681433ec2a617225dfb907
yeongjun Kim <ehdwjs500dnj@naver.com>
2023-01-20 01:52:46 +0900

    Apply Material UI

--- Node.js/management/src/App.js
+++ Node.js/management/src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
 import Customer from "./components/Customer";
+import Paper from "@mui/material/Paper"; //외부를 감싸기 위해 사용하는 컴포넌트
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import { withStyles } from "@mui/material"; //css를 작성할 수 있도록 하는 라이브러리
 
 const customers = [
   {
@@ -30,21 +37,35 @@ const customers = [
 
 function App() {
   return (
-    <div>
-      {customers.map((customer) => {
-        return (
-          <Customer
-            key={customer.id}
-            id={customer.id}
-            image={customer.image}
-            name={customer.name}
-            birthday={customer.birsday}
-            gender={customer.gender}
-            job={customer.job}
-          />
-        );
-      })}
-    </div>
+    <Paper id="paper">
+      <Table id="container">
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((customer) => {
+            return (
+              <Customer
+                key={customer.id}
+                id={customer.id}
+                image={customer.image}
+                name={customer.name}
+                birthday={customer.birsday}
+                gender={customer.gender}
+                job={customer.job}
+              />
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
--- Node.js/management/src/components/Customer.js
+++ Node.js/management/src/components/Customer.js
@@ -1,30 +1,18 @@
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
 function Customer({ name, birthday, gender, job, id, image }) {
   return (
-    <div>
-      <CustomerProfile name={name} id={id} image={image} />
-      <CustomerInfo birthday={birthday} gender={gender} job={job} />
-    </div>
+    <TableRow>
+      <TableCell>{id}</TableCell>
+      <TableCell>
+        <img src={image} alt="profile" />
+      </TableCell>
+      <TableCell>{name}</TableCell>
+      <TableCell>{birthday}</TableCell>
+      <TableCell>{gender}</TableCell>
+      <TableCell>{job}</TableCell>
+    </TableRow>
   );
 }
 
-function CustomerProfile({ image, name, id }) {
-  return (
-    <div>
-      <img src={image} alt="profile" />
-      <h2>
-        {name}({id})
-      </h2>
-    </div>
-  );
-}
-
-function CustomerInfo({ birthday, gender, job }) {
-  return (
-    <div>
-      <p>{birthday}</p>
-      <p>{gender}</p>
-      <p>{job}</p>
-    </div>
-  );
-}
 export default Customer;
