bb66e3a3670377961cb89553368572de2328f0ab
yeongjun Kim <ehdwjs500dnj@naver.com>
2023-01-22 23:59:41 +0900

    Add Progress

--- Node.js/management/client/src/App.js
+++ Node.js/management/client/src/App.js
@@ -8,9 +8,21 @@ import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import { withStyles } from "@mui/material"; //css를 작성할 수 있도록 하는 라이브러리
 import { useEffect, useState } from "react";
+import Stack from "@mui/material/Stack";
+import CircularProgress from "@mui/material/CircularProgress";
 
 function App() {
   const [customersDate, setCustomersDate] = useState([]);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 10));
+    }, 800);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   useEffect(() => {
     (async () => {
@@ -20,37 +32,46 @@ function App() {
   }, []);
 
   return (
-    <Paper id="paper">
-      <Table id="container">
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {customersDate
-            ? customersDate.map((customer) => {
-                return (
-                  <Customer
-                    key={customer.id}
-                    id={customer.id}
-                    image={customer.image}
-                    name={customer.name}
-                    birthday={customer.birsday}
-                    gender={customer.gender}
-                    job={customer.job}
-                  />
-                );
-              })
-            : ""}
-        </TableBody>
-      </Table>
-    </Paper>
+    <>
+      <Stack spacing={2} direction="row">
+        <CircularProgress variant="determinate" value={25} />
+        <CircularProgress variant="determinate" value={50} />
+        <CircularProgress variant="determinate" value={75} />
+        <CircularProgress variant="determinate" value={100} />
+        <CircularProgress variant="determinate" value={progress} />
+      </Stack>
+      <Paper id="paper">
+        <Table id="container">
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customersDate
+              ? customersDate.map((customer) => {
+                  return (
+                    <Customer
+                      key={customer.id}
+                      id={customer.id}
+                      image={customer.image}
+                      name={customer.name}
+                      birthday={customer.birsday}
+                      gender={customer.gender}
+                      job={customer.job}
+                    />
+                  );
+                })
+              : ""}
+          </TableBody>
+        </Table>
+      </Paper>
+    </>
   );
 }
 
