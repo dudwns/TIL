0efacc7f5b78b053e8b75d564c8f2fded7c6fcf0
yeongjun Kim <ehdwjs500dnj@naver.com>
2023-03-22 18:20:50 +0900

    Custom App

--- NextJS/nextjs-intro/components/NavBar.js
+++ NextJS/nextjs-intro/components/NavBar.js
@@ -4,26 +4,45 @@ import { useRouter } from "next/router";
 export default function NavBar() {
   const router = useRouter(); //router 객체 내부의 정보를 알 수 있다.
   return (
-    <>
-      <nav>
-        <Link href="/">
-          <span className={router.pathname === "/" ? "active" : ""}>Home</span>
+    <nav>
+      <img src="/vercel.svg" />
+      <div>
+        <Link href="/" className={router.pathname === "/" ? "active" : ""}>
+          Home
         </Link>
-        <Link href="/about">
-          <span className={router.pathname === "/about" ? "active" : ""}>About</span>
+        <Link href="/about" className={router.pathname === "/about" ? "active" : ""}>
+          About
         </Link>
-        <style jsx>{`
-          a {
-            text-decoration: none;
-          }
-          .active {
-            color: tomato;
-          }
-          span {
-            background-color: white;
-          }
-        `}</style>
-      </nav>
-    </>
+      </div>
+      <style jsx>{`
+        //styled jsx : 스타일의 적용 범위를 오직 해당 컴포넌트 내부로 한정한다.
+
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
+    </nav>
   );
 }
--- NextJS/nextjs-intro/src/pages/_app.js
+++ NextJS/nextjs-intro/src/pages/_app.js
@@ -1,18 +1,12 @@
+import Layout from "components/Layout";
 import "../styles/globals.css";
-import NavBar from "components/NavBar";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <NavBar />
-      <Component {...pageProps} />
-      <style jsx global>
-        {`
-          span {
-            color: white;
-          }
-        `}
-      </style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
