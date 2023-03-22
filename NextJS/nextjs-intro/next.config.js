bf2406cf48fe8ed8388ba90e6fc0c3bd8a63f59a
yeongjun Kim <ehdwjs500dnj@naver.com>
2023-03-22 18:22:04 +0900

    redirects & rewrites

--- NextJS/nextjs-intro/next.config.js
+++ NextJS/nextjs-intro/next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  // redirect 시킴, URL 변경
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*", //유저가 접속하는 URL
+        destination: "/new-blog/:path*", //실제로 보낼 URL
+        permanent: false, //기록을 남기는지 여부
+      },
+    ];
+  },
+
+  // rewrites는 redirect를 시키긴 하지만, URL이 변하지 않음
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies", // API KEY를 숨김
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
